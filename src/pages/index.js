487ecd2aaef5904c7c467336706229797549c29a
smitesh <smiteshsawantz@gmail.com>
2023-05-06 12:38:54 -0700

    add gatsbyjs and template readme

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Hero from "../components/hero"
+
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <Hero />
+    </Layout>
+  )
+}
+
+export default IndexPage
